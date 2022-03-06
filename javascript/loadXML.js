4fc3f56650bdce5df1e1665e72c04ca7cd9c35e2
Fanis Siampos <fsiamp@gmail.com>
2022-03-06 20:37:49 +0200

    Modified placeholder color

--- javascript/loadXML.js
+++ javascript/loadXML.js
@@ -1,25 +1,3 @@
-$(document).ready(function(){
-	$.ajax({
-		type: "GET",
-		url: "quotes/quotes.txt",
-		dataType: "text",
-		success: function(data){
-            var lines = data.split("\n");
-
-            //Random item number
-            var r = Math.floor(Math.random() * lines.length);
-
-            //Get random line
-            var line = lines[r].split("|");
-            $('#author').val(" " + line[0]);
-			$('#quote').val(line[1]);
-		    },
-		error: function() {
-		    alert("An error occurred while processing quote dataset.");
-		}
-	});
-});
-
 function reply_click() {
     $(document).ready(function(){
 	$.ajax({
