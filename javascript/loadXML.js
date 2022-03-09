43ee5285a84756822ed047889a1ea547770fa56b
Fanis Siampos <fsiamp@gmail.com>
2022-03-10 00:26:13 +0200

    Separated quote dataset

--- javascript/loadXML.js
+++ javascript/loadXML.js
@@ -1,8 +1,11 @@
 function reply_click() {
+	var items = Array(278622, 314116, 505219, 549217, 719437, 758978, 802673, 836547, 964054, 968903);
+	var item = items[Math.floor(Math.random()*items.length)];
+
     $(document).ready(function(){
 	$.ajax({
 		type: "GET",
-		url: "quotes/quotes.txt",
+		url: "quotes/" + item + ".txt",
 		dataType: "text",
 		success: function(data){
             var lines = data.split("\n");
