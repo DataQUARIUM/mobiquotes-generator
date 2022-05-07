caaef048df0dc243586abac71801746121a9c4fb
Fanis Siampos <fsiamp@gmail.com>
2022-05-07 20:50:11 +0300

    Added new ruby script

---
+++ mobiquotes.rb
@@ -0,0 +1,45 @@
+puts "If I ever reach heaven I expect to find three wonders there: first, to meet some I had not thought to see there; second, to miss some I had expected to see there; and third, the greatest wonder of all, to find myself there."
+sleep 5
+puts "Leave your past and live for today"
+sleep 5
+puts "You pays your money and you takes your choice."
+sleep 5
+puts "My wife tells me she doesn't care what I do when I'm away  as long as I'm not enjoying it."
+sleep 5
+puts "I'm not going to change I'm very stubborn in this way. I am what I am."
+sleep 5
+puts "A trap, like a goodstory, needs to hint at greater things without revealing themuntil the prey is snared."
+sleep 5
+puts "It was not courage, exactly; the object was not valor. Rather, they were too frightened to be cowards."
+sleep 5
+puts "Didn't know where I was going. Didn't care where I ended up. I knew I'd be okay though."
+sleep 5
+puts "Getting sober just exploded my life. Now I have a much clearer sense of myself and what I can and can't do. I am more successful than I have ever been. I feel very positive where I never did before, and I think that's all a direct result of getting sober."
+sleep 5
+puts "No man will swim ashore and take his baggage with him."
+sleep 5
+puts "Believe not in the reality, but believe in the possibility."
+sleep 5
+puts "We can create a new kind of politics: kinder, more respectful, but courageous, too."
+sleep 5
+puts "I wasn't a major in political science for nothing, so I understood the politics of beauty and the politics of race when it comes to the fashion industry."
+sleep 5
+puts "Your reputation has no duplicate. You are one till the end of time. Once it is damaged, a fresh personality cannot spring from the old one."
+sleep 5
+puts "Everyone is given the potential to excel, whether you do or not is entirely up to you."
+sleep 5
+puts "You are the richest person if you own the treasures of both the solitude and the togetherness!"
+sleep 5
+puts "She gave me this look – she might have been watching from a lifeboat as the ship went down. Or maybe it was the other way around."
+sleep 5
+puts "The human brain is an incredible pattern-matching machine."
+sleep 5
+puts "Sentiments that glorify humanity know no racial distinction."
+sleep 5
+puts "I pulled myself from his mind, day by day, piece by piece, memory by memory, until there was nothing of Ruby left to weigh him down or keep him bound to my side.."
+sleep 5
+puts "We have so far to go to realize our human potential for compassion, altruism, and love."
+sleep 5
+puts "You deserve a longer letter than this"
+sleep 5
+puts "Reading is a technology for perspective-taking. When someone else's thoughts are in your head, you are observing the world from that person's vantage point."
